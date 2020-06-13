18e49979e8d6c514b5ebf98cf2b3c0b541c5c5cf
Kyky Farly Vallendra <lorem.ipsum@gmail.com>
2020-06-13 15:50:01 +0700

    Updates

---
+++ precache-manifest.24e39e0df40d22274a675f18e84b57f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76f9e2d536642160f8a82bb3798051b6",
+    "url": "/media-player/index.html"
+  },
+  {
+    "revision": "36d8ac988bea607b19c6",
+    "url": "/media-player/static/css/main.d13af4f5.chunk.css"
+  },
+  {
+    "revision": "2278a3fa15df3063009f",
+    "url": "/media-player/static/js/2.90875ddc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/media-player/static/js/2.90875ddc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36d8ac988bea607b19c6",
+    "url": "/media-player/static/js/main.9afe1680.chunk.js"
+  },
+  {
+    "revision": "695cd8cb465794116741",
+    "url": "/media-player/static/js/runtime-main.d8365ff9.js"
+  }
+]);
